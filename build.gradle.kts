49fe6aff0520eec018ca8ea273cc9908ecff565a
Mathias Karlsson <Mathias.Karlsson@xlent.se>
2018-11-27 23:31:28 +0100

    Add jetbrains/exposed dependency to gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 val logback_version: String by project
 val ktor_version: String by project
 val kotlin_version: String by project
+val exposed_version: String by project
 
 plugins {
     application
@@ -25,6 +26,7 @@ repositories {
 
 dependencies {
     compile("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin_version")
+    compile("org.jetbrains.exposed:exposed:$exposed_version")
     compile("io.ktor:ktor-server-netty:$ktor_version")
     compile("ch.qos.logback:logback-classic:$logback_version")
     compile("io.ktor:ktor-server-core:$ktor_version")
